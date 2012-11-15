d668768c1d8d7d64c398435749d398ef97253783
Luka Mirosevic <luka@goonbee.com>
2012-11-15 13:15:08 +0100

    Initial Commit

---
+++ PIngTest/GBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GBAppDelegate.h
+//  PIngTest
+//
+//  Created by Luka Mirosevic on 15/11/2012.
+//  Copyright (c) 2012 Goonbee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PIngTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PIngTest
+//
+//  Created by Luka Mirosevic on 15/11/2012.
+//  Copyright (c) 2012 Goonbee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GBAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GBAppDelegate class]));
+    }
+}
